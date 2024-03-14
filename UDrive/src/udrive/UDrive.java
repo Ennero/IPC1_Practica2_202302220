d0fa27a56220f99ad8d417f7902bc72d539a292b
ElEnnero <159963896+ElEnnero@users.noreply.github.com>
2024-03-13 22:25:45 -0600

    Inicio con la interfaz gráfica

---
+++ UDrive/src/udrive/UDrive.java
@@ -0,0 +1,21 @@
+
+package udrive;
+
+/**
+ *
+ * @author Enner
+ */
+public class UDrive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio inicio= new Inicio();
+        inicio.setLocationRelativeTo(null);
+        inicio.setResizable(false);
+        inicio.setVisible(true);
+    }
+    
+}
